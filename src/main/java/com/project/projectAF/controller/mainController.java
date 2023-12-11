ffac97c9e11c0d5bc89b05671c7b5c75f6154248
kiyeong Lee <pyocosss@naver.com>
2023-12-11 16:53:22 +0900

    first commit

--- src/main/java/com/project/projectAF/controller/mainController.java
+++ src/main/java/com/project/projectAF/controller/mainController.java
@@ -3,6 +3,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 //test
+//test2
 @Controller
 public class mainController {
     @GetMapping("/mainView")
